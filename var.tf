b8b8950f53db32031f5bf8395ddd43a59c13ce72
dbesong <ashu.emerence@yahoo.com>
2024-06-23 13:07:48 -0600

    shared lib

---
+++ var.tf
@@ -0,0 +1,21 @@
+def call(String stageName){
+
+    if ("${stageName}" == "Build")
+       {
+        sh "mvn clean package"
+       }
+    else if ("${stageName}" == "SonarQube Report")
+       {
+        sh "mvn clean sonar:sonar"
+       }
+
+    else if ("${stageName}" == "upload into Nexus")
+       {
+        sh "mvn clean deploy"
+       }
+
+
+
+
+
+}
